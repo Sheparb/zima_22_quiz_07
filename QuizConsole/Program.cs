2b3a63242b9ee4ddda2a5529c5a6885e2387a34a
@ahermansan <aherman@san.edu.pl>
2022-10-10 14:58:57 +0200

    10.10.2022

---
+++ QuizConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("WITAMY W GRZE QUIZ");
+Console.WriteLine("Spróbuj odpowiedziec na 5 pytań");
+Console.WriteLine();
+Console.WriteLine("Pytanie za 100 pkt");
+Console.WriteLine();
+Console.WriteLine("Które miasto jest stolicą Ukrainy?");
+Console.WriteLine();
+Console.WriteLine("1. Lwów");`  
+Console.WriteLine("2. Kijów");
+Console.WriteLine("3. Charków");
+Console.WriteLine("4. Odessa");
+Console.WriteLine();
+Console.Write("Nacisnij 1, 2, 3 lub 4 => ");
