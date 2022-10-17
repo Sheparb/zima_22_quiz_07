6ddd38e7090b9bfa9875ea739e999435638dcf11
@ahermansan <aherman@san.edu.pl>
2022-10-17 14:23:22 +0200

    17.10.2022

--- QuizConsole/Program.cs
+++ QuizConsole/Program.cs
@@ -1,13 +1,32 @@
-﻿Console.WriteLine("WITAMY W GRZE QUIZ");
-Console.WriteLine("Spróbuj odpowiedziec na 5 pytań");
+﻿using QuizConsole;
+
+
+Console.WriteLine("Hello World");
+
+
+//var pytanie = new Question();
+//pytanie.Id = 1;
+//pytanie.Content = "Jakie miasto jest stolicą Ukrainy?";
+//pytanie.Category = 100;
+//pytanie.Answer1 = new Answer();
+//pytanie.Answer1.Id = 1;
+//pytanie.Answer1.Content = "Kijów";
+//pytanie.Answer1.IsCorrect = true;
+//pytanie.Answer2 = new Answer();
+//pytanie.Answer2.Id = 2;
+//pytanie.Answer2.Content = "Odessa";
+//pytanie.Answer2.IsCorrect = false;
+//pytanie.Answer3 = new Answer();
+//pytanie.Answer3.Id = 3;
+//pytanie.Answer3.Content = "Charków";
+//pytanie.Answer3.IsCorrect = false;
+//pytanie.Answer4 = new Answer();
+//pytanie.Answer4.Id = 4;
+//pytanie.Answer4.Content = "Lwów";
+//pytanie.Answer4.IsCorrect = false;
+
+
+
+
 Console.WriteLine();
-Console.WriteLine("Pytanie za 100 pkt");
-Console.WriteLine();
-Console.WriteLine("Które miasto jest stolicą Ukrainy?");
-Console.WriteLine();
-Console.WriteLine("1. Lwów");`  
-Console.WriteLine("2. Kijów");
-Console.WriteLine("3. Charków");
-Console.WriteLine("4. Odessa");
-Console.WriteLine();
-Console.Write("Nacisnij 1, 2, 3 lub 4 => ");
+
---
+++ QuizConsole/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizConsole
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int Category { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+
+}
